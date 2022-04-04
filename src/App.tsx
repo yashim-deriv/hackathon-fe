6b10e409fa292d21634d4f93ae847fb83010179c
yashimwong <yashimwong@gmail.com>
2022-04-04 11:40:43 +0800

    feat: initial commit main pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home.page";
+import History from "./pages/history.page";
+import Rank from "./pages/rank.page";
+
 const App = () => {
     return (
-        <div className="flex">
-            <h1 className="text-5xl font-semibold">Hello World</h1>
-        </div>
+        <React.Fragment>
+            <h1>Navigation</h1>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/history" element={<History />} />
+                    <Route path="/rank" element={<Rank />} />
+                </Routes>
+            </BrowserRouter>
+        </React.Fragment>
     );
 };
 
