7389d4c0d024f2a9e3d62a966179cf2cee2a7669
yashim-deriv <yashim@deriv.com>
2022-04-04 11:09:49 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div className="flex">
+            <h1 className="text-5xl font-semibold">Hello World</h1>
+        </div>
+    );
+};
+
+export default App;
